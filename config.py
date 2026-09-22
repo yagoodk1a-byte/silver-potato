2806b810389051c866c6cb3c7f010f97664673a3
yagoodk1a-byte <yagoodk1a@gmail.com>
2026-09-22 16:30:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-BOT_TOKEN = ("8358575962:AAEc4tLrbOOm5Dbm1pttawejOZ0NOgnQryQ")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('8358575962:AAEc4tLrbOOm5Dbm1pttawejOZ0NOgnQryQ')
+ 
