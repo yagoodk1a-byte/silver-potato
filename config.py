6355b8e12369a1f0b00e5df6b2f4b99d79aafffd
yagoodk1a-byte <yagoodk1a@gmail.com>
2026-09-22 16:51:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,2 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 TOKEN = os.getenv("8358575962:AAEc4tLrbOOm5Dbm1pttawejOZ0NOgnQryQ")
 
-if not TOKEN:
-    raise RuntimeError("❌ BOT_TOKEN не найден в переменных окружения")
-
-print("✅ BOT_TOKEN найден")
